a443ac6823fcaba346dfaa1417b1b32e2e79963c
youz2me <kynhun20@gachon.ac.kr>
2025-10-15 00:58:56 +0900

    [Feat] #16 - SetlistFeature DTO 구현
    - FetchConcertSetlist: 셋리스트 상세 조회
    - FetchSetlistSongList: 셋리스트 곡 목록 조회
    
    typealias를 활용한 배열 타입 선언

---
+++ Livith-iOS/Projects/Network/Sources/DTO/SetlistFeature/FetchConcertSetlist.swift
@@ -0,0 +1,37 @@
+//
+//  FetchConcertSetlist.swift
+//  network
+//
+//  Created by Youjin Lee on 10/14/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+// MARK: - 17. 특정 콘서트 셋리스트 상세 조회
+
+import Foundation
+
+public extension DTO.Response {
+    struct FetchConcertSetlist: Decodable {
+        public let id: Int
+        public let title: String
+        public let imageURL: String
+        public let type: String
+        public let startDate: String
+        public let endDate: String
+        public let status: String
+        public let venue: String
+        public let artist: String
+
+        enum CodingKeys: String, CodingKey {
+            case id
+            case title
+            case imageURL = "imgUrl"
+            case type
+            case startDate
+            case endDate
+            case status
+            case venue
+            case artist
+        }
+    }
+}
