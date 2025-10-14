9d68bcf62e06e503da62c25ffa0ee90caf9e3d15
youz2me <kynhun20@gachon.ac.kr>
2025-10-15 00:59:54 +0900

    [Feat] #16 - UserFeature DTO 구현
    - DeleteUser: 회원 탈퇴
    - RequestLogout: 로그아웃
    - UpdateUserNickname: 닉네임 변경
    
    사용자 관리 관련 Request/Response 구조 정립

---
+++ Livith-iOS/Projects/Network/Sources/DTO/UserFeature/DeleteUser.swift
@@ -0,0 +1,9 @@
+//
+//  DeleteUser.swift
+//  network
+//
+//  Created by Youjin Lee on 10/15/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+// MARK: - 35. 회원탈퇴
---
+++ Livith-iOS/Projects/Network/Sources/DTO/UserFeature/UpdateUserNickname.swift
@@ -0,0 +1,9 @@
+//
+//  UpdateUserNickname.swift
+//  network
+//
+//  Created by Youjin Lee on 10/14/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+// MARK: - 33. 닉네임 수정
