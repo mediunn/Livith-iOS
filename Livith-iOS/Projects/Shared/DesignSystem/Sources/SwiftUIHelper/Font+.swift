4b5b469c76c37a7f4a93498def022addbdfc1041
youz2me <kynhun20@gachon.ac.kr>
2025-11-02 20:35:15 +0900

    [Fix] #18 - Font 익스텐션 내에 Notosans 열거형 선언되도록 수정

--- Livith-iOS/Projects/Shared/DesignSystem/Sources/SwiftUIHelper/Font+.swift
+++ Livith-iOS/Projects/Shared/DesignSystem/Sources/SwiftUIHelper/Font+.swift
@@ -12,6 +12,69 @@ public extension Font {
     
     // MARK: - Notosans
     
+    /// `Notosans`는 다양한 텍스트 스타일을 정의하는 열거형입니다.
+    enum Notosans {
+        case title
+        case headSemibold, headMedium, headRegular
+        case body1Semibold
+        case body2Semibold, body2Medium, body2Regular
+        case body3Semibold, body3Medium, body3Regular
+        case body4Semibold, body4Medium, body4Regular
+        case caption1Bold, caption1Semibold, caption1Regular
+        case caption2Semibold, caption2Regular
+        
+        
+        /// 해당 스타일의 폰트 이름을 반환합니다.
+        var fontName: String {
+            switch self {
+            case .title, .caption1Bold:
+                return DesignSystemFontFamily.NotoSansKR.bold.name
+            case .headSemibold, .body1Semibold, .body2Semibold, .body3Semibold, .caption1Semibold, .caption2Semibold:
+                return DesignSystemFontFamily.NotoSansKR.semiBold.name
+            case .headMedium, .body2Medium, .body3Medium, .body4Medium:
+                return DesignSystemFontFamily.NotoSansKR.medium.name
+            default:
+                return DesignSystemFontFamily.NotoSansKR.regular.name
+            }
+        }
+        
+        /// 해당 스타일의 폰트 크기를 반환합니다.
+        public var size: CGFloat {
+            switch self {
+            case .title: return 26
+            case .headSemibold, .headMedium, .headRegular: return 22
+            case .body1Semibold: return 18
+            case .body2Semibold, .body2Medium, .body2Regular: return 16
+            case .body3Semibold, .body3Medium, .body3Regular: return 15
+            case .body4Semibold, .body4Medium, .body4Regular: return 14
+            case .caption1Bold, .caption1Semibold, .caption1Regular: return 12
+            case .caption2Semibold, .caption2Regular: return 10
+            }
+        }
+        
+        /// 해당 스타일의 자간(Kerning)을 반환합니다.
+        public var kerning: CGFloat {
+            return size * -0.05
+        }
+        
+        /// 해당 스타일의 줄 높이(Line Height)를 반환합니다.
+        public var lineHeight: CGFloat {
+            switch self {
+            case .caption1Bold, .caption1Semibold:
+                return size * 1.3
+            case .caption1Regular, .caption2Semibold, .caption2Regular:
+                return size * 1.2
+            default:
+                return size * 1.4
+            }
+        }
+        
+        /// 해당 스타일의 베이스라인 오프셋(Baseline Offset)을 반환합니다.
+        public var baselineOffset: CGFloat {
+            return (lineHeight - size) / 3
+        }
+    }
+    
     /// `Notosans` 폰트를 반환하는 정적 메서드.
     ///
     /// 사용 예시:
@@ -27,7 +90,7 @@ public extension Font {
 // MARK: - Text Modifier for Notosans
 
 public struct NotosansModifier: ViewModifier {
-    let style: Notosans
+    let style: Font.Notosans
     
     public func body(content: Content) -> some View {
         content
@@ -46,7 +109,7 @@ public extension View {
     /// Text("Hello")
     ///     .notosans(.headLarge)
     /// ```
-    func notosans(_ style: Notosans) -> some View {
+    func notosans(_ style: Font.Notosans) -> some View {
         modifier(NotosansModifier(style: style))
     }
 }
