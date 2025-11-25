ce753ae0378bc9398bd1ea716661d8c59ce33fe6
youz2me <kynhun20@gachon.ac.kr>
2025-11-25 15:11:38 +0900

    [Refactor] 배너, 콘서트, 검색 섹션 fetch DTO livithnetwork로 이동

--- Projects/Core/LivithNetwork/Sources/DTO/SearchFeature/FetchConcertList.swift
+++ Projects/Core/LivithNetwork/Sources/DTO/SearchFeature/FetchConcertList.swift
@@ -10,6 +10,8 @@
 
 import Foundation
 
+import livithnetwork
+
 public extension DTO.Response {
     struct FetchConcertList: Decodable {
         public let data: [Concert]
--- Projects/Core/LivithNetwork/Sources/DTO/SearchFeature/FetchSearchSectionList.swift
+++ Projects/Core/LivithNetwork/Sources/DTO/SearchFeature/FetchSearchSectionList.swift
@@ -10,6 +10,8 @@
 
 import Foundation
 
+import livithnetwork
+
 public extension DTO.Response {
     typealias FetchSearchSectionList = [SearchSection]
 
