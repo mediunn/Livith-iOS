1f833764dbcbe006cb5da5a653381abaa82524e9
JinUng41 <ingjwjw@naver.com>
2025-12-04 22:31:28 +0900

    [Chore] #37 - 파일 삭제 및 주석 수정

--- Projects/Core/Auth/Sources/AuthError.swift
+++ Projects/Core/Auth/Sources/AuthError.swift
@@ -2,8 +2,8 @@
 //  AuthError.swift
 //  Auth
 //
-//  Created by 김진웅 on 12/3/24.
-//  Copyright © 2024 Livith. All rights reserved.
+//  Created by 김진웅 on 12/3/25.
+//  Copyright © 2025 Livith. All rights reserved.
 //
 
 import Foundation
