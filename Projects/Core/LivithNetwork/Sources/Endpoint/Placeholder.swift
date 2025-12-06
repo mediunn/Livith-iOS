ae420c3d106ffa1c8dae62898606b762264e72de
JinUng41 <ingjwjw@naver.com>
2025-12-06 20:58:29 +0900

    [Setting] #45 - 네트워크 모듈 폴더링

---
+++ Projects/Core/LivithNetwork/Sources/Endpoint/Placeholder.swift
@@ -0,0 +1,9 @@
+//
+//  Placeholder.swift
+//  LivithNetwork
+//
+//  Created by 김진웅 on 12/6/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Core/LivithNetwork/Sources/Token/Token_Placeholder.swift
@@ -0,0 +1,9 @@
+//
+//  Token_Placeholder.swift
+//  LivithNetwork
+//
+//  Created by 김진웅 on 12/6/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+import Foundation
