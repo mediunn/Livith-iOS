78d44e54ee94be35d5f0552638f58a51f2194c42
youz2me <kynhun20@gachon.ac.kr>
2025-12-11 02:04:57 +0900

    [Feat] #50 - CheckNicknameDuplicate DTO 구현

---
+++ Projects/Core/LivithNetwork/Sources/DTO/UserFeature/CheckNicknameDuplicate.swift
@@ -0,0 +1,17 @@
+//
+//  CheckNicknameDuplicate.swift
+//  LoginData
+//
+//  Created by 김진웅 on 11/10/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+// MARK: - 34. 닉네임 중복 확인
+
+import Foundation
+
+public extension DTO.Response {
+    struct CheckNicknameDuplicate: Decodable {
+        let available: Bool
+    }
+}
