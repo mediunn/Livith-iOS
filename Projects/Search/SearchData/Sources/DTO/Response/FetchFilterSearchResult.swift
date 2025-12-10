49604d73b86c6fb2468373db3f408425c716f5e7
youz2me <kynhun20@gachon.ac.kr>
2025-12-11 00:53:49 +0900

    [Fix] #48 - Search 모듈 DTO 및 Entity 옵셔널 타입 수정

--- Projects/Search/SearchData/Sources/DTO/Response/FetchFilterSearchResult.swift
+++ Projects/Search/SearchData/Sources/DTO/Response/FetchFilterSearchResult.swift
@@ -27,12 +27,12 @@ public extension DTO.Response {
             public let status: String
             public let posterURL: String
             public let artist: String
-            public let daysLeft: Int?
-            public let ticketSite: String
-            public let ticketURL: String
+            public let daysLeft: Int
+            public let ticketSite: String?
+            public let ticketURL: String?
             public let venue: String
             public let introduction: String
-            public let label: String
+            public let label: String?
 
             enum CodingKeys: String, CodingKey {
                 case id
--- Projects/Search/SearchData/Sources/Mapper/SearchMapper.swift
+++ Projects/Search/SearchData/Sources/Mapper/SearchMapper.swift
@@ -15,8 +15,7 @@ public class SearchMapper {
     func toDomain(from response: DTO.Response.FetchFilterSearchResult) -> SearchResultEntity {
         let concerts = response.data.compactMap { concert -> ConcertEntity? in
             guard let status = ConcertStatus(rawValue: concert.status),
-                  let posterURL = URL(string: concert.posterURL),
-                  let ticketURL = URL(string: concert.ticketURL) else {
+                  let posterURL = URL(string: concert.posterURL) else {
                 return nil
             }
 
@@ -25,13 +24,13 @@ public class SearchMapper {
                 title: concert.title,
                 artist: concert.artist,
                 status: status,
-                daysLeft: concert.daysLeft ?? 0,
+                daysLeft: concert.daysLeft,
                 startDate: concert.startDate,
                 endDate: concert.endDate,
                 posterURL: posterURL,
                 venue: concert.venue,
                 ticketSite: concert.ticketSite,
-                ticketURL: ticketURL,
+                ticketURL: URL(string: concert.ticketURL ?? ""),
                 introduction: concert.introduction,
                 label: concert.label
             )
