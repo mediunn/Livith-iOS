88b7c7c5b8f65c53826e06e21fcf40659d3e1246
youz2me <kynhun20@gachon.ac.kr>
2025-12-11 16:47:26 +0900

    [Refactor] #50 - UserRepository 메서드명 변경

--- Projects/User/UserDomain/Sources/Repository/UserRepository.swift
+++ Projects/User/UserDomain/Sources/Repository/UserRepository.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public protocol UserRepository {
     func checkNicknameDuplicate(nickname: String) async throws(UserError) -> Bool
-    func changeNewNickname(nickname: String) async throws(UserError) -> String
+    func updateUserNickname(nickname: String) async throws(UserError) -> String
     func deleteUser(reason: String) async throws(UserError) -> Void
     func logoutSession() async throws(UserError) -> Void
 }
