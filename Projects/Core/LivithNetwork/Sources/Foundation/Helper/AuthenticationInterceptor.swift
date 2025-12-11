5954f4746f6461f321401787cd4c31771b27ef22
JinUng41 <ingjwjw@naver.com>
2025-12-11 16:23:55 +0900

    [Feat] #53 - 리퀘스트 인터셉터 구현

---
+++ Projects/Core/LivithNetwork/Sources/Foundation/Helper/AuthenticationInterceptor.swift
@@ -0,0 +1,60 @@
+//
+//  AuthenticationInterceptor.swift
+//  LivithNetwork
+//
+//  Created by 김진웅 on 12/11/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+import Foundation
+
+import Alamofire
+
+public final class AuthenticationInterceptor: RequestInterceptor {
+    private let tokenService: TokenService
+    
+    public init(tokenService: TokenService) {
+        self.tokenService = tokenService
+    }
+    
+    public func adapt(
+        _ urlRequest: URLRequest,
+        for session: Session,
+        completion: @escaping (Result<URLRequest, any Error>) -> Void
+    ) {
+        Task {
+            do {
+                let accessToken = try await tokenService.getAccessToken()
+
+                var adaptedRequest = urlRequest
+                adaptedRequest.headers.add(.authorization(bearerToken: accessToken))
+                completion(.success(adaptedRequest))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func retry(
+        _ request: Request,
+        for session: Session,
+        dueTo error: Error,
+        completion: @escaping (RetryResult) -> Void
+    ) {
+        guard let response = request.task?.response as? HTTPURLResponse,
+              response.statusCode == 401 
+        else {
+            completion(.doNotRetry)
+            return
+        }
+
+        Task {
+            do {
+                try await tokenService.refresh()
+                completion(.retry)
+            } catch {
+                completion(.doNotRetryWithError(error))
+            }
+        }
+    }
+}
