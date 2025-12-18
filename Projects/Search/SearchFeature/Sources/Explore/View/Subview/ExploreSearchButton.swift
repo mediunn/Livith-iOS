a6ccc00ee0e8364b05a6df564ce2f551702366cd
JinUng41 <ingjwjw@naver.com>
2025-12-18 17:30:40 +0900

    [Refactor] #69 - 불필요한 ZStack 제거

--- Projects/Search/SearchFeature/Sources/Explore/View/Subview/ExploreSearchButton.swift
+++ Projects/Search/SearchFeature/Sources/Explore/View/Subview/ExploreSearchButton.swift
@@ -11,34 +11,34 @@ import SwiftUI
 import DSKit
 
 struct ExploreSearchButton: View {
+    @EnvironmentObject private var router: ExploreRouter
+    
     var body: some View {
-        ZStack {
-            Button {
-                // TODO: 검색 뷰로 이동
-            } label: {
-                HStack {
-                    Text("찾고 있는 콘서트나 가수를 검색하세요")
-                        .notosans(.body3Medium)
-                        .foregroundColor(.livithColor(.black50))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.leading, 12)
-                    
-                    Image.livithIcon(.searchLineDefault)
-                        .resizable()
-                        .frame(width: 36, height: 36)
-                        .padding(.vertical, 8)
-                        .padding(.trailing, 12)
-                }
-                .background(
-                    Color.livithColor(.black90)
-                )
-                .cornerRadius(10)
-                .padding(.horizontal, 16)
-                .padding(.top, 12)
-                .padding(.bottom, 8)
+        Button {
+            // TODO: 검색 뷰로 이동
+//                router.push(.search)
+        } label: {
+            HStack {
+                Text("찾고 있는 콘서트나 가수를 검색하세요")
+                    .notosans(.body3Medium)
+                    .foregroundColor(.livithColor(.black50))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 12)
+                
+                Image.livithIcon(.searchLineDefault)
+                    .resizable()
+                    .frame(width: 36, height: 36)
+                    .padding(.vertical, 8)
+                    .padding(.trailing, 12)
             }
+            .background(
+                Color.livithColor(.black90)
+            )
+            .cornerRadius(10)
         }
-        
+        .padding(.horizontal, 16)
+        .padding(.top, 12)
+        .padding(.bottom, 8)
     }
 }
 
