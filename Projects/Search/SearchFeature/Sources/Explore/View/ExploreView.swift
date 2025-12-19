1a64421125349cbd0c417899e6c16b6f746bfd1b
JinUng41 <ingjwjw@naver.com>
2025-12-20 02:06:12 +0900

    [Style] #69 - 하단 여백 수정

--- Projects/Search/SearchFeature/Sources/Explore/View/ExploreView.swift
+++ Projects/Search/SearchFeature/Sources/Explore/View/ExploreView.swift
@@ -125,7 +125,7 @@ private extension ExploreView {
     
     enum Constants {
         static let bannerHeight: CGFloat = 365
-        static let emptySpaceHeight: CGFloat = 60
+        static let emptySpaceHeight: CGFloat = 120
     }
 }
 
