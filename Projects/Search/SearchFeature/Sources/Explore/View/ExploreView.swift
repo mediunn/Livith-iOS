b0c721e25ad01bdbc213f351f1ed075a82d7c730
JinUng41 <ingjwjw@naver.com>
2025-12-20 01:09:48 +0900

    [Refactor] #69 - 가독성을 위한 코드 분리

--- Projects/Search/SearchFeature/Sources/Explore/View/ExploreView.swift
+++ Projects/Search/SearchFeature/Sources/Explore/View/ExploreView.swift
@@ -17,54 +17,70 @@ struct ExploreView: View {
     @State private var banners: [Banner] = Banner.mocks
     @State private var concertSections: [ConcertSection] = ConcertSection.mocks
     @State private var scrollOffset: CGFloat = 0
-
+    
     var body: some View {
         VStack {
             ExploreLogoView()
             
             ZStack(alignment: .top) {
-                ExploreSearchButton(onTap: {
-                    
-                    // TODO: Router를 이용한 검색 화면 이동
-                    
-                })
-                .zIndex(2)
-                .background(
-                    scrollOffset > Constants.bannerHeight - 60
-                    ? Color.livithColor(.black100)
-                    : Color.clear
-                )
+                searchButton
+                    .zIndex(2)
+                    .background(
+                        scrollOffset > Constants.bannerHeight - 60
+                        ? Color.livithColor(.black100)
+                        : Color.clear
+                    )
                 
-                ScrollView(showsIndicators: false) {
-                    VStack(spacing: 0) {
-                        BannerSectionView(currentPage: $currentPage, banners: banners)
-                            .frame(height: Constants.bannerHeight)
-                            .background(
-                                GeometryReader { proxy in
-                                    Color.clear.onChange(of: proxy.frame(in: .named(Literals.scrollCoordinateName)).minY) {
-                                        scrollOffset = -proxy.frame(in: .named(Literals.scrollCoordinateName)).minY
-                                    }
-                                }
-                            )
-                        
-                        ForEach(concertSections, id: \.id) { section in
-                            ConcertSectionView(
-                                concertSection: section,
-                                onConcertTap: { concert in
-                                    
-                                    // TODO: Router를 이용한 콘서트 상세 화면 이동 + Concert 전달
-                                    
-                                }
-                            )
-                            .padding(.top, 36)
-                            .padding(.leading, 16)
-                        }
-                    }
-                }
-                .coordinateSpace(name: Literals.scrollCoordinateName)
+                scrollContent
             }
         }
         .background(Color.livithColor(.black100))
+    }
+}
+
+// MARK: - UI Components
+
+private extension ExploreView {
+    var searchButton: some View {
+        ExploreSearchButton(onTap: {
+            // TODO: Router를 이용한 검색 화면 이동
+        })
+    }
+    
+    var scrollContent: some View {
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 0) {
+                bannerView
+                
+                ForEach(concertSections, id: \.id) { section in
+                    concertSectionRow(for: section)
+                        .padding(.top, 36)
+                        .padding(.leading, 16)
+                }
+            }
+        }
+        .coordinateSpace(name: Literals.scrollCoordinateName)
+    }
+    
+    var bannerView: some View {
+        BannerSectionView(currentPage: $currentPage, banners: banners)
+            .frame(height: Constants.bannerHeight)
+            .background(
+                GeometryReader { proxy in
+                    Color.clear.onChange(of: proxy.frame(in: .named(Literals.scrollCoordinateName)).minY) {
+                        scrollOffset = -proxy.frame(in: .named(Literals.scrollCoordinateName)).minY
+                    }
+                }
+            )
+    }
+    
+    func concertSectionRow(for section: ConcertSection) -> some View {
+        ConcertSectionView(
+            concertSection: section,
+            onConcertTap: { concert in
+                // TODO: Router를 이용한 콘서트 상세 화면 이동 + Concert 전달
+            }
+        )
     }
 }
 
