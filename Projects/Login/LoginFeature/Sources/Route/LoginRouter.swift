33e05e26c71eb5ba970c89cc2b8d9f1a02cb047c
JinUng41 <ingjwjw@naver.com>
2025-12-21 14:43:52 +0900

    [Refactor] #76 - 컴포넌트 수정 및 라우터 적용

--- Projects/Login/LoginFeature/Sources/Route/LoginRouter.swift
+++ Projects/Login/LoginFeature/Sources/Route/LoginRouter.swift
@@ -29,6 +29,16 @@ final class LoginRouter: Router {
         case .login:
             return AnyView(LoginView())
             
+        case .loginForbidden:
+            return AnyView(
+                ErrorSheetView(
+                    title: "탈퇴 후 7일이 지나지 않았어요",
+                    message: "7일이 지난 후 다시 시도해주세요"
+                ) { [weak self] in
+                    self?.dismissFullScreen()
+                }
+            )
+            
         case .terms(let tempUser):
             self.tempUser = tempUser
             
@@ -43,7 +53,16 @@ final class LoginRouter: Router {
             return AnyView(NicknameSettingView(store: store))
             
         case .signupFailed:
-            return AnyView(SignupFailedSheetView())
+            return AnyView(
+                ErrorSheetView(
+                    title: "오류가 발생했어요!",
+                    message: "잠시 후 다시 시도해주세요",
+                    confirmTitle: "로그인으로 돌아가기"
+                ) { [weak self] in
+                    self?.popToRoot()
+                    self?.dismissFullScreen()
+                }
+            )
             
         case .safari(let url):
             let safariView = SafariView(url: url) { [weak self] in
