6ebe1834d0a3d54774527b9d0e3c98b15b8183f8
JinUng41 <ingjwjw@naver.com>
2025-12-24 16:13:25 +0900

    [Feat] #87 - 홈 라우터 추가

---
+++ Projects/Home/HomeFeature/Sources/Router/HomeRootView.swift
@@ -0,0 +1,34 @@
+//
+//  HomeRootView.swift
+//  HomeFeature
+//
+//  Created by 김진웅 on 12/24/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+import SwiftUI
+
+import Router
+
+public struct HomeRootView: View {
+    @State private var router: HomeRouter = .init()
+    
+    public init() {}
+    
+    public var body: some View {
+        NavigationStack(path: $router.path) {
+            router.view(to: .home, with: .push)
+                .navigationDestination(for: HomeRoute.self) { route in
+                    router.view(to: route, with: .push)
+                        .navigationBarHidden(true)
+                }
+        }
+        .sheet(item: $router.sheet) { route in
+            router.view(to: route, with: .sheet)
+        }
+        .fullScreenCover(item: $router.fullScreenCover) { route in
+            router.view(to: route, with: .fullScreen)
+        }
+        .environmentObject(router)
+    }
+}
---
+++ Projects/Home/HomeFeature/Sources/Router/HomeRouter.swift
@@ -0,0 +1,28 @@
+//
+//  HomeRouter.swift
+//  HomeFeature
+//
+//  Created by 김진웅 on 12/24/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+import SwiftUI
+import Observation
+
+import Router
+
+@Observable
+final class HomeRouter: Router {
+    typealias R = HomeRoute
+    
+    var path: NavigationPath = .init()
+    var sheet: HomeRoute?
+    var fullScreenCover: HomeRoute?
+    
+    func view(to route: HomeRoute, with style: PresentationStyle) -> AnyView {
+        switch route {
+        case .home:
+            AnyView(EmptyView())
+        }
+    }
+}
