82ba1904b2a9009918676adaedfc111974a6dc6f
youz2me <kynhun20@gachon.ac.kr>
2025-12-31 16:40:27 +0900

    [Refactor] #94 LIVD-55 - FlowLayout 캐시 적용으로 중복 레이아웃 계산 제거

--- Projects/DSKit/Sources/Component/FlowLayout.swift
+++ Projects/DSKit/Sources/Component/FlowLayout.swift
@@ -10,6 +10,13 @@ import SwiftUI
 
 public struct FlowLayout: Layout {
 
+    // MARK: - Cache
+
+    public struct Cache {
+        var size: CGSize = .zero
+        var positions: [CGPoint] = []
+    }
+
     // MARK: - Property
 
     private let spacing: CGFloat
@@ -22,15 +29,19 @@ public struct FlowLayout: Layout {
 
     // MARK: - Layout
 
-    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+    public func makeCache(subviews: Subviews) -> Cache {
+        Cache()
+    }
+
+    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout Cache) -> CGSize {
         let result = arrangeSubviews(proposal: proposal, subviews: subviews)
+        cache.size = result.size
+        cache.positions = result.positions
         return result.size
     }
 
-    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        let result = arrangeSubviews(proposal: proposal, subviews: subviews)
-
-        for (index, position) in result.positions.enumerated() {
+    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout Cache) {
+        for (index, position) in cache.positions.enumerated() {
             subviews[index].place(
                 at: CGPoint(x: bounds.minX + position.x, y: bounds.minY + position.y),
                 proposal: .unspecified
