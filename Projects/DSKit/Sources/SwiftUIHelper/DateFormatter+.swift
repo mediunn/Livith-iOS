a99baaea6dd13ed3914dee5b19e42b4a48ff7eb8
youz2me <kynhun20@gachon.ac.kr>
2026-01-05 00:05:40 +0900

    [Refactor] #126 LIVD-94 - DateFormatter+.swift에서 DateFormatterFactory 사용하도록 변경

--- Projects/DSKit/Sources/SwiftUIHelper/DateFormatter+.swift
+++ Projects/DSKit/Sources/SwiftUIHelper/DateFormatter+.swift
@@ -8,22 +8,12 @@
 
 import Foundation
 
+import LivithFoundation
+
 public extension DateFormatter {
-    static let fullDate: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy.MM.dd"
-        return formatter
-    }()
-
-    static let shortDate: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MM.dd"
-        return formatter
-    }()
-
     static func formatDateRange(from startDate: Date, to endDate: Date) -> String {
         let calendar = Calendar.current
-        let startDateString = fullDate.string(from: startDate)
+        let startDateString = DateFormatterFactory.dotDate.string(from: startDate)
 
         if calendar.isDate(startDate, inSameDayAs: endDate) {
             return startDateString
@@ -33,10 +23,10 @@ public extension DateFormatter {
         let endYear = calendar.component(.year, from: endDate)
 
         if startYear == endYear {
-            let endShortString = shortDate.string(from: endDate)
+            let endShortString = DateFormatterFactory.shortDate.string(from: endDate)
             return "\(startDateString)~\(endShortString)"
         } else {
-            let endDateString = fullDate.string(from: endDate)
+            let endDateString = DateFormatterFactory.dotDate.string(from: endDate)
             return "\(startDateString)~\(endDateString)"
         }
     }
