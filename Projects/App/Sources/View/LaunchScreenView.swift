23655f4c147fafd2c130e00dff999e820480bdef
JinUng41 <ingjwjw@naver.com>
2026-01-04 16:34:27 +0900

    [Style] #127 - 스플래쉬 텍스트 삭제

--- Projects/App/Sources/View/LaunchScreenView.swift
+++ Projects/App/Sources/View/LaunchScreenView.swift
@@ -17,21 +17,11 @@ struct LaunchScreenView: View {
                 .resizable()
                 .ignoresSafeArea()
             
-            VStack(spacing: 0) {
-                Spacer()
-                
-                Image.livithImage(.livithLogo)
-                    .resizable()
-                    .aspectRatio(204/52, contentMode: .fit)
-                    .frame(height: 44)
-                    .padding(.bottom, 24)
-                
-                Text("분산되어 있는 내한 정보를 한번에")
-                    .notosans(.body1Semibold)
-                    .foregroundStyle(Color.livithColor(.black50))
-                    
-                Spacer()
-            }
+            Image.livithImage(.livithLogo)
+                .resizable()
+                .aspectRatio(204/52, contentMode: .fit)
+                .frame(height: 48)
+                .padding(.bottom, 24)
         }
     }
 }
