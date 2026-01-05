3e76728176e8d528d177f8b0a0fa7950ed8ac302
youz2me <kynhun20@gachon.ac.kr>
2026-01-05 15:00:55 +0900

    [Refactor] #126 LIVD-94 - iOS 7+ DateFormatter thread-safe로 인한 NSLock 제거

--- Projects/Core/LivithFoundation/Sources/DateFormatter/DateFormatterService.swift
+++ Projects/Core/LivithFoundation/Sources/DateFormatter/DateFormatterService.swift
@@ -11,19 +11,13 @@ import Foundation
 // MARK: - DateFormatterService
 
 public enum DateFormatterService {
-    private static let lock = NSLock()
-
     /// String을 Date로 변환합니다.
     public static func date(from string: String, type: DateFormatType) -> Date? {
-        lock.lock()
-        defer { lock.unlock() }
-        return type.formatter.date(from: string)
+        type.formatter.date(from: string)
     }
 
     /// Date를 String으로 변환합니다.
     public static func string(from date: Date, type: DateFormatType) -> String {
-        lock.lock()
-        defer { lock.unlock() }
-        return type.formatter.string(from: date)
+        type.formatter.string(from: date)
     }
 }
