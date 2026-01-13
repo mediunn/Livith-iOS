30dcdf1f4c6818b4772c59d4df3c712291115cd1
youz2me <kynhun20@gachon.ac.kr>
2026-01-13 18:28:47 +0900

    [Add] #143 - Coordinator 모듈을 Core로 이동

---
+++ Projects/Core/Coordinator/Sources/Route.swift
@@ -0,0 +1,11 @@
+//
+//  Route.swift
+//  Coordinator
+//
+//  Created by Youjin Lee on 12/27/25.
+//  Copyright © 2025 Livith. All rights reserved.
+//
+
+import Foundation
+
+public protocol Route: Hashable {}
