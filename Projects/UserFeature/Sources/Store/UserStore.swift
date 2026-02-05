2e2d59aa68a8b06f98cee549496fe44cef3f92b0
youz2me <kynhun20@gachon.ac.kr>
2026-02-06 02:42:07 +0900

    [Feat] #195 - 마이페이지 선호 장르/아티스트 fetch 로직 구현

--- Projects/UserFeature/Sources/Store/UserStore.swift
+++ Projects/UserFeature/Sources/Store/UserStore.swift
@@ -13,33 +13,54 @@ import Domain
 
 struct UserState {
     var nickname: String = ""
-    var genres: [String] = []
-    var artists: [String] = []
+    var genres: [PreferredGenre] = []
+    var artists: [PreferredArtist] = []
 
     var hasGenreData: Bool { !genres.isEmpty }
     var hasArtistData: Bool { !artists.isEmpty }
 }
 
 enum UserIntent {
-    case fetchNickname
+    case fetchUserInfo
     case _fetchUserResult(Result<User, Error>)
+    case _fetchGenreListResult(Result<[PreferredGenre], Error>)
+    case _fetchArtistListResult(Result<[PreferredArtist], Error>)
 }
 
 final class UserStore: ObservableObject {
     @Published private(set) var state = UserState()
 
     @Injected private var userRepository: UserRepository
+    @Injected private var preferenceRepository: PreferenceRepository
 
     @MainActor
     func send(_ intent: UserIntent) {
         switch intent {
-        case .fetchNickname:
+        case .fetchUserInfo:
             performFetchUser()
-            
+            performFetchGenreList()
+            performFetchArtistList()
+
         case ._fetchUserResult(let result):
             switch result {
             case .success(let user):
                 state.nickname = user.nickname
+            case .failure:
+                break
+            }
+
+        case ._fetchGenreListResult(let result):
+            switch result {
+            case .success(let genres):
+                state.genres = genres
+            case .failure:
+                break
+            }
+
+        case ._fetchArtistListResult(let result):
+            switch result {
+            case .success(let artists):
+                state.artists = artists
             case .failure:
                 break
             }
@@ -57,6 +78,28 @@ private extension UserStore {
                 await send(._fetchUserResult(.success(user)))
             } catch {
                 await send(._fetchUserResult(.failure(error)))
+            }
+        }
+    }
+
+    func performFetchGenreList() {
+        Task {
+            do {
+                let genres = try await preferenceRepository.fetchUserPreferredGenreList()
+                await send(._fetchGenreListResult(.success(genres)))
+            } catch {
+                await send(._fetchGenreListResult(.failure(error)))
+            }
+        }
+    }
+
+    func performFetchArtistList() {
+        Task {
+            do {
+                let artists = try await preferenceRepository.fetchUserPreferredArtistList()
+                await send(._fetchArtistListResult(.success(artists)))
+            } catch {
+                await send(._fetchArtistListResult(.failure(error)))
             }
         }
     }
