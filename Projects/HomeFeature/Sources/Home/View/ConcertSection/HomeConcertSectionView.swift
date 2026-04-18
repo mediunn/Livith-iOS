c478e1f58b32a66f6dcdaabaf9fd51b1ce825853
JinUng41 <ingjwjw@naver.com>
2026-04-18 17:09:00 +0900

    [Style] LIVD-341 - 홈 레이아웃 조정

--- Projects/HomeFeature/Sources/Home/View/ConcertSection/HomeConcertSectionView.swift
+++ Projects/HomeFeature/Sources/Home/View/ConcertSection/HomeConcertSectionView.swift
@@ -149,7 +149,7 @@ private extension HomeConcertSectionView {
 private extension HomeConcertSectionView {
     enum Constants {
         static let emptySpaceHeight: CGFloat = 210
-        static let sectionTopPadding: CGFloat = 28
+        static let sectionTopPadding: CGFloat = 32
         static let sectionLeadingPadding: CGFloat = 16
         static let loadingMinHeight: CGFloat = 240
     }
