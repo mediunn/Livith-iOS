d6a4243616f0511da27ea09ed6b775f5b2debec2
youz2me <kynhun20@gachon.ac.kr>
2026-04-28 18:36:10 +0900

    [Refactor] LIVD-360 - ConcertGenre POP, INDIE 재정의 및 한국어/영어 텍스트 분리

--- Projects/Domain/Sources/Entity/Search/ConcertGenre.swift
+++ Projects/Domain/Sources/Entity/Search/ConcertGenre.swift
@@ -12,9 +12,8 @@ public enum ConcertGenre: String, CaseIterable {
     case jpop = "JPOP"
     case rockMetal = "ROCK_METAL"
     case rapHiphop = "RAP_HIPHOP"
-    case classicJazz = "CLASSIC_JAZZ"
-    case acoustic = "ACOUSTIC"
-    case electronic = "ELECTRONIC"
+    case pop = "POP"
+    case indie = "INDIE"
     case all = "ALL"
 }
 
@@ -27,12 +26,27 @@ public extension ConcertGenre {
             return "락/메탈"
         case .rapHiphop:
             return "랩/힙합"
-        case .classicJazz:
-            return "클래식/재즈"
-        case .acoustic:
-            return "어쿠스틱"
-        case .electronic:
-            return "일렉트로닉"
+        case .pop:
+            return "팝"
+        case .indie:
+            return "인디"
+        case .all:
+            return "전체"
+        }
+    }
+
+    var genreEnglishText: String {
+        switch self {
+        case .jpop:
+            return "J-POP"
+        case .rockMetal:
+            return "ROCK/METAL"
+        case .rapHiphop:
+            return "RAP/HIPHOP"
+        case .pop:
+            return "POP"
+        case .indie:
+            return "INDIE"
         case .all:
             return "전체"
         }
