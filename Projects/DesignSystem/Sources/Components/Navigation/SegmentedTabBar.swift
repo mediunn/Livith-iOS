da53a0886c43dce293f5f0cb4b97def6063efe42
JinUng41 <ingjwjw@naver.com>
2026-07-17 10:00:59 +0900

    [Fix] LIVD-438 - 관심 콘서트 없을 때 홈 세그먼트 배경 단차 수정
    홈 SegmentedTabBar 배경을 clear로 바꿔 빈 상태 black90 화면과 맞춘다.

--- Projects/DesignSystem/Sources/Components/Navigation/SegmentedTabBar.swift
+++ Projects/DesignSystem/Sources/Components/Navigation/SegmentedTabBar.swift
@@ -133,13 +133,23 @@ public struct SegmentedTabBar: View {
 
             tabBarContent
         }
-        .background(.livithColor(.black100))
+        .background(barBackgroundColor)
     }
 }
 
 // MARK: - Content
 
 private extension SegmentedTabBar {
+    var barBackgroundColor: Color {
+        switch type {
+        case .home:
+            // 홈은 관심 콘서트 유무에 따라 화면 배경(black90/black100)이 달라지므로 부모 배경을 비춘다.
+            return .clear
+        case .detail:
+            return .livithColor(.black100)
+        }
+    }
+
     var tabBarContent: some View {
         Group {
             if type.isScrollable {
